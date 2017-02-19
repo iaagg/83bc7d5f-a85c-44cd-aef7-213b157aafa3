c133f6fbcbf74942fd61657262bc6cc0ab71b0a3
Alexey Getman <alexeygetman@Alexeys-MacBook-Pro.local>
2017-02-20 01:42:39 +0300

    Currency collection view setup in progress

--- Revolut-test/Exchange/Submodules/Currency/Interactor/CurrencyInteractor.m
+++ Revolut-test/Exchange/Submodules/Currency/Interactor/CurrencyInteractor.m
@@ -3,10 +3,18 @@
 #import "PONSO_Wallet.h"
 #import "PONSO_Currency.h"
 
+static NSInteger const kRevolutCarouselSectionsCount = 3;
+
 @implementation CurrencyInteractor
 
 - (void)makeDataSourceForCurrencyCollectionViewWithWallet:(PONSO_Wallet *)wallet {
+    NSArray *currencies = wallet.currencies;
+    NSMutableArray *dataSource = [NSMutableArray array];
     
+    for (int i = 0; i < kRevolutCarouselSectionsCount ; i++) {
+        dataSource add
+    }
+
 }
 
 @end
